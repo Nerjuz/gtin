c50a56d2189f9b8a986c89b506ddd6600607a1d3
Nerijus Zutautas <nerijus.zutautas@metro-markets.de>
2024-01-17 16:51:23 +0200

    more gtins

--- index.php
+++ index.php
@@ -4,19 +4,16 @@ require_once 'vendor/autoload.php';
 
 use Real\Validator\Gtin;
 
-$gtin8 = $gtin12 = $gtin14 ='';
+$gtin = [];
 
-while (!Gtin\Factory::isValid($gtin14)){
-    $gtin14 = round(rand(10000000000000,99999999999999),1);
+while (!isset($gtin['gtin14']) && !Gtin\Factory::isValid($gtin['gtin14'])){
+    $gtin['gtin14'] = round(rand(10000000000000,99999999999999),1);
 }
 
-while (!Gtin\Factory::isValid($gtin12)){
-    $gtin12 = round(rand(100000000000,999999999999),1);
+while (!isset($gtin['gtin12']) && !Gtin\Factory::isValid($gtin['gtin12'])){
+    $gtin['gtin12'] = round(rand(100000000000,999999999999),1);
 }
-while (!Gtin\Factory::isValid($gtin8)){
-    $gtin8 = round(rand(10000000,99999999),1);
+while (!isset($gtin['gtin8']) && !Gtin\Factory::isValid($gtin['gtin8'])){
+    $gtin['gtin8'] = round(rand(10000000,99999999),1);
 }
-dump ('s');
-echo PHP_EOL.$gtin8.PHP_EOL;
-echo PHP_EOL.$gtin12.PHP_EOL;
-echo PHP_EOL.$gtin14.PHP_EOL;
+dump ( $gtin['gtin14']);
